6783f5086541e2d7e934e52f72a4ec8be03e1aed
Karthik Marupeddi <karthikmarupeddi@Karthiks-iMac.local>
2016-07-10 11:42:07 -0500

    fixed vaadin

--- src/main/java/wservices/WservicesApplication.java
+++ src/main/java/wservices/WservicesApplication.java
@@ -1,9 +1,11 @@
 package wservices;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class WservicesApplication {
 
 	public static void main(String[] args) {
